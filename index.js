bf7143d52762ddbb0cdc3b11bdbc369b4f7b1959
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-12 13:31:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function destructivelyRemoveLastKitten(){
       return kittens.slice(0, kittens.length - 1);
   }
   
-  removeFirstKitten(){
+  function removeFirstKitten(){
     return kittens.slice(0);
   }
