fc484823c604b087518da874adb04b48bd7b6bcf
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-12 13:30:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+//var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+  
+  function destructivelyRemoveFirstKitten(){
+    kittens.shift();
+  }
+  
+  function appendKitten(name){
+    newKittens = [kittens, ...name];
+    return newKittens;
+  }
+  
+  function prependKitten(name){
+     newKittens = [...name, kittens];
+    return newKittens;
+  }
+  
+  function removeLastKitten(){
+      return kittens.slice(0, kittens.length - 1);
+  }
+  
+  removeFirstKitten(){
+    return kittens.slice(0);
+  }
